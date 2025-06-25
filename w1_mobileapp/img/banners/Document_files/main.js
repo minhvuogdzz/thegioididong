8390ed753918ffd184b66c2d48a9921e13485617
minhvuogdzz <ougvn.it2@gmail.com>
2025-06-25 11:53:13 +0700

    Initial commit: Added thegioididong into w1_mobileapp folder

---
+++ w1_mobileapp/img/banners/Document_files/main.js
@@ -0,0 +1,17 @@
+/**Toán tử số học
+ * Cộng
+ * Trừ
+ * Nhân
+ * Luỹ thừa
+ * Chia
+ * Chia lấy dư
+ * Tăng 1 giá trị số
+ * Giảm 1 giá trị số
+ */
+
+var a = 1;
+var b = 2;
+
+var c = b + c; // 3
+
+console.log(c);
